c0173c81279de880b88a8e285df0ceb477782c08
Kaushal <82136218+1NT18IS076@users.noreply.github.com>
2021-10-19 13:25:40 +0530

    Update configure-user.sh

--- configure-user.sh
+++ configure-user.sh
@@ -6,5 +6,7 @@ git config user.email "1nt18is076.kaushal@nmit.ac.in"
 echo "User name set to:"
 git config user.name
 
-echo "\nUser email set to:"
+echo ""
+
+echo "User email set to:"
 git config user.email
