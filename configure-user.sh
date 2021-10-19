79ef96338dcb6238240775371f10017a450913bc
Kaushal <1nt18is076.kaushal@nmit.ac.in>
2021-10-19 12:54:13 +0530

    add lab1

---
+++ configure-user.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git config user.name "Kaushal"
+git config user.email "1nt18is076.kaushal@nmit.ac.in"
+
+echo "User name set to:"
+git config user.name
+
+echo "\nUser email set to:"
+git config user.email
