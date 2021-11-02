31ec1b8db228468034e06bdea66ab92f9c47bbfb
Kaushal <1nt18is076.kaushal@nmit.ac.in>
2021-11-02 21:40:16 +0530

    update lab3

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -9,18 +9,13 @@ import android.widget.TextView;
 import android.view.View;
 import android.widget.Toast;
 
-/*
-* ScriptEngineManager manager = new ScriptEngineManager();
-* ScriptEngine engine = manager.getEngineByName("js");
-* System.out.println(engine.eval("4*5"));
-* */
-
 public class MainActivity extends AppCompatActivity {
 
     TextView result;
 
-    String op1;
-    String op2;
+    String op1 = "";
+    String op2 = "";
+    String op = "";
 
     boolean isSecondNum = false;
 
@@ -33,22 +28,94 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void numberEvent(View view) {
-        String number = result.getText().toString();
+//        String number = result.getText().toString();
 
         Button b = (Button) view;
 
-        number += b.getText().toString();
+//        number += b.getText().toString();
 
         if (isSecondNum) {
-            op2 = number;
+            op2 += b.getText().toString();
         } else {
-            op1 = number;
+            op1 += b.getText().toString();
         }
 
-        result.setText(number);
+        result.setText(op1 + op + op2);
+    }
 
-//        Toast.makeText(getApplicationContext(), b.getText().toString(), Toast.LENGTH_SHORT).show();
+    public void clearEvent(View view) {
+        result.setText("");
+        op1 = "";
+        op2 = "";
+        op = "";
 
+        isSecondNum = false;
+    }
 
+    public void flipSignEvent(View view) {
+        if (op2 != "" || op != "") {
+            result.setText("");
+        } else {
+            if (op1.charAt(0) == '-') {
+                op1 = op1.substring(1);
+            } else {
+                op1 = "-" + op1;
+            }
+
+            result.setText(op1);
+        }
+    }
+
+    public void percentEvent(View view) {
+        if (op2 != "") {
+            result.setText("");
+        } else {
+            op1 = String.valueOf(Float.valueOf(op1) / 100);
+            result.setText(op1);
+        }
+    }
+
+    public void operatorEvent(View view) {
+        Button b = (Button) view;
+
+        op = b.getText().toString();
+
+        isSecondNum = true;
+
+        result.setText(op1 + op + op2);
+    }
+
+    public void evalEvent(View view) {
+        try {
+            float operand1 = Float.parseFloat(op1);
+            float operand2 = Float.parseFloat(op2);
+
+            float answer = 0;
+
+            switch (op) {
+                case "+":
+                    answer = operand1 + operand2;
+                    break;
+                case "-":
+                    answer = operand1 - operand2;
+                    break;
+                case "*":
+                    answer = operand1 * operand2;
+                    break;
+                case "/":
+                    answer = operand1 / operand2;
+                    break;
+            }
+
+            String output = String.valueOf(String.format("%.2f", answer));
+
+            result.setText(output);
+            op1 = output;
+            op2 = "";
+            isSecondNum = false;
+            op = "";
+        } catch (Exception e) {
+            result.setText("");
+        }
     }
 }
