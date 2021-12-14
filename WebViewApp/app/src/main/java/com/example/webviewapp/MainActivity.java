06100a3ac9761d9252ac448815bcb09633f25be7
Kaushal <1nt18is076.kaushal@nmit.ac.in>
2021-12-14 09:13:29 +0530

    add webview app

---
+++ WebViewApp/app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+
+//        WebView webView = new WebView(getApplicationContext());
+//
+//        setContentView(webView);
+
+        webView.loadUrl("https://kaush.me");
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setDomStorageEnabled(true);
+    }
+}
